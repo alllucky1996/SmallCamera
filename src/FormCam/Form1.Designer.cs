626f173305e3e164b0a0edc5ebbafaa0de097729
dũng it <lmetuoi@myschool.host>
2020-09-17 17:41:42 +0700

    init demo

---
+++ src/FormCam/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FormCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCam = new System.Windows.Forms.PictureBox();
+            this.btnAction = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCam
+            // 
+            this.picCam.Location = new System.Drawing.Point(60, 84);
+            this.picCam.Name = "picCam";
+            this.picCam.Size = new System.Drawing.Size(677, 328);
+            this.picCam.TabIndex = 0;
+            this.picCam.TabStop = false;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(212, 30);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(75, 23);
+            this.btnAction.TabIndex = 1;
+            this.btnAction.Text = "Stop";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.picCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCam;
+        private System.Windows.Forms.Button btnAction;
+    }
+}
+
